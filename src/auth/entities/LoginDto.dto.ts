51ec1490cf5c054506901a0b12a41b6866d2cdaa
Max Houston Ramirez Martel <maxr524@gmail.com>
2024-02-05 22:35:17 -0500

    add(doc): add exameples to swagger

--- src/auth/entities/LoginDto.dto.ts
+++ src/auth/entities/LoginDto.dto.ts
@@ -1,10 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 //LoginDto
 export class LoginDto {
+  @ApiProperty({
+    example: 'houston@gmail.com',
+    description: 'The email of the user',
+  })
   @IsString()
   email: string;
-
+  @ApiProperty({
+    example: 'password',
+    description: 'The password of the user',
+  })
   @IsString()
   password: string;
 }
--- src/post/dto/register-post.dto.ts
+++ src/post/dto/register-post.dto.ts
@@ -1,40 +1,59 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsBoolean, IsOptional, IsArray } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({ example: 'Post Title', description: 'The title of the post' })
   @IsString()
   title: string;
 
+  @ApiProperty({
+    example: 'Post content',
+    description: 'The content of the post',
+  })
   @IsString()
   content: string;
 
+  @ApiProperty({
+    example: ['healthy', 'finanzas'],
+    description: 'The categories of the post',
+  })
   @IsArray()
   categories: [string];
 
+  @ApiProperty({
+    example: true,
+    description: 'its true if this post is public',
+  })
   @IsBoolean()
   isDraft: boolean;
 }
 
 export class UpdatePostsDto {
-  @IsString()
-  @IsOptional()
-  name?: string;
-
+  @ApiProperty({ example: 'Post Title', description: 'The title of the post' })
   @IsString()
   @IsOptional()
   title?: string;
 
-  @IsString()
-  @IsOptional()
-  author?: string;
-
+  @ApiProperty({
+    example: 'Post content',
+    description: 'The content of the post',
+  })
   @IsString()
   @IsOptional()
   content?: string;
 
+  @ApiProperty({
+    example: ['healthy', 'finanzas'],
+    description: 'The categories of the post',
+  })
   @IsArray()
   @IsOptional()
   categories?: [string];
 
+  @ApiProperty({
+    example: true,
+    description: 'its true if this post is public',
+  })
   @IsBoolean()
   @IsOptional()
   isDraft: boolean;
